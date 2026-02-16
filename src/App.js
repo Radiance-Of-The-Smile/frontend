e6aff5f6a92d633b80f8a49f7817fc581e02017d
ibbee <89143882+ibbee@users.noreply.github.com>
2026-02-17 00:36:02 +0500

    Update App.js
    updating api end point with a variable instead of hardcoding it

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import axios from "axios";
 import './App.css';
 
 const api = axios.create({
-  baseURL: `http://localhost:8000/`
+  baseURL: process.env.REACT_APP_API_URL//`http://localhost:8000/`
 });
 
 const App = () => {
